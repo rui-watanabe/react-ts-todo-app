c9ac8121c183d5039a4cf1e958c073fda0477b33
rui-watanabe <wataru610806@gmail.com>
2020-07-19 17:41:05 +0900

    setting tslint and prettier

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,17 +1,15 @@
-import React, { PropsWithChildren } from 'react';
-import { ColumnContainer, ColumnTitle } from './styles';
+import React, { PropsWithChildren } from "react"
+import { ColumnContainer, ColumnTitle } from "./styles"
 
-interface ColumnProps 
-{
-  text?: string;
-};
+interface ColumnProps {
+  text?: string
+}
 
-export const Column = ({ text, children }: PropsWithChildren<ColumnProps> ) => 
-{
-  return(
+export const Column = ({ text, children }: PropsWithChildren<ColumnProps>) => {
+  return (
     <ColumnContainer>
-      <ColumnTitle>{ text }</ColumnTitle>
-      { children }
+      <ColumnTitle>{text}</ColumnTitle>
+      {children}
     </ColumnContainer>
   )
-};
+}
